64977a17b536384a3785815ca6d4ffd09d479bb0
Andrews Rezende <andrewsrezende@gmail.com>
2023-02-06 22:04:45 -0300

    Bootcamp e Dev

---
+++ src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -0,0 +1,80 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Bootcamp {
+
+
+    private String nome;
+    private String descricao;
+    private final LocalDate dataInicial = LocalDate.now();
+    private final LocalDate dataFinal = dataInicial.plusDays(45);
+    private Set<Dev> devsIncritos = new HashSet<>();
+    private Set<Conteudo> conteudos = new LinkedHashSet<>();
+
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataInicial() {
+        return this.dataInicial;
+    }
+
+
+    public LocalDate getDataFinal() {
+        return this.dataFinal;
+    }
+
+
+    public Set<Dev> getDevsIncritos() {
+        return this.devsIncritos;
+    }
+
+    public void setDevsIncritos(Set<Dev> devsIncritos) {
+        this.devsIncritos = devsIncritos;
+    }
+
+    public Set<Conteudo> getConteudos() {
+        return this.conteudos;
+    }
+
+    public void setConteudos(Set<Conteudo> conteudos) {
+        this.conteudos = conteudos;
+    }
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Bootcamp)) {
+            return false;
+        }
+        Bootcamp bootcamp = (Bootcamp) o;
+        return Objects.equals(nome, bootcamp.nome) && Objects.equals(descricao, bootcamp.descricao) && Objects.equals(dataInicial, bootcamp.dataInicial) && Objects.equals(dataFinal, bootcamp.dataFinal) && Objects.equals(devsIncritos, bootcamp.devsIncritos) && Objects.equals(conteudos, bootcamp.conteudos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, descricao, dataInicial, dataFinal, devsIncritos, conteudos);
+    }
+
+}
